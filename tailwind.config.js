a5740ef40eabd4a5593e4e20aec2ab3a1070cb7c
migsilva89 <migdrum@gmail.com>
2021-12-01 16:23:26 +0100

    test vscode git

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        bluep: {
+        blue: {
           "50": "#108109104",
           "100": "#f6f5fa",
           "200": "#d0cde5",
