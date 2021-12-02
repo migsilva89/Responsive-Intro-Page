5b95a24e5d1ae647b5d92f7a7ca2854de540d447
migsilva89 <migdrum@gmail.com>
2021-12-02 19:55:03 +0100

    Mobile version fixing

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Vue 3 + Vite
+# Vue 3 + Vite + Tailwindcss first time.
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+Sign form with active state.
 
-## Recommended IDE Setup
+Design and requests provided by: https://www.frontendmentor.io/ 
 
-- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
+
+
--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -16,27 +16,27 @@
       <!-- BANNER -->
       <div style="background-image: url(images/images/bg-intro-desktop.png)" class="bg-redp-500">
         <div class="flex items-center min-h-screen " >
-          <div class="px-8 mx-auto max-w-screen-2xl rounded-xl h-screen flex justify-center items-center">
+          <div class="md:px-32 mx-auto max-w-screen-2xl rounded-xl flex items-center">
             <div class="max-w-screen-xl flex flex-col md:flex-row mx-auto">
               <!-- PRIMEIRA COLUNA  -->
-              <h1 class="text-left md:pt-48 md:pr-28 h-32 md:h-auto md:w-1/2 text-white text-5xl font-black ">
+              <h1 class="md:text-left md:pt-48 md:pr-28 pb-5 text-center h-auto md:w-1/2 text-white px-8 md:text-5xl text-4xl font-extrabold ">
                 Lear to code by watching others.
-                <p class="text-left pt-8 md:h-auto text-white text-base font-light">
+                <p class="md:text-left pt-8 md:h-auto text-white text-base font-light">
                   See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
                   but understanding how developers think is invaluable.
                 </p>
               </h1>
               <!-- DIREITA COLUNA  -->
-              <div class="flex items-center  justify-center p-8 sm:p-8 md:w-1/2">
-                <div class="max-w-full">
-                  <h1 class="bg-bluep-500 shadow-xl rounded-lg block w-full px-4 py-4 mt-4 mb-4 text-base font-black text-center text-white">
+              <div class="flex items-center justify-center md:w-1/2">
+                <div class="max-w-full p-4">
+                  <h1 class="bg-bluep-500 shadow-xl rounded-lg block w-full lg:px-26 md-16 px-16 py-4 mt-4 mb-4 text-base font-bold text-center text-white">
                     Try it free 7 days <span class="font-thin">then $20/mo. thereafter</span>
                   </h1>
 
                   <!-- NAME FIELD -->
                   <div class="bg-white shadow-xl rounded-lg block w-full px-4 py-2 sm:p-8 mt-4 mb-4">
                     <div>
-                      <label class="block text-sm"></label>
+                      <label class="block text-sm pb-4 "></label>
                       <div class="relative flex">
                         <input 
                             type="text" 
@@ -130,16 +130,21 @@
                     <div class="submit">
                     <button 
                         @click="validateForm"
-                        class="font-extrabold uppercase w-full px-8 py-2 mt-4 text-sm leading-8 text-center text-white transition-colors duration-150 bg-greenb-500 border border-transparent rounded-lg active:bg-blue-600 hover:bg-green-300 hover:shadow-md focus:outline-none focus:shadow-outline-black">
+                        class="font-bold uppercase w-full px-8 py-2 mt-4 text-sm leading-8 text-center text-white transition-colors duration-150 bg-greenb-500 border border-transparent rounded-lg active:bg-blue-600 hover:bg-green-300 hover:shadow-md focus:outline-none focus:shadow-outline-black">
                         Claim your free trial
                     </button>
                     </div>
 
                     <!-- TERMS AND CONDITIONS -->
-                    <div class="mt-4 text-center px-8">
+                    <div class="mt-6 pb-4 md:pb-0 text-center px-8">
                       <p class="text-xs text-gray-400"> By clicking the button, you are agreeing to our 
                         <a href="#" class="text-redp-500 font-normal hover:underline"> Terms and Services.</a>
                         </p>
+                    </div>
+                    <div class="attribution lg:py-16 text-center fixed left-0 bottom-0 right-0 text-white">Coded by
+                      <a href="https://github.com/migsilva89/" target="blank"> 
+                        <span class="text-red-100 hover:text-bluep-500">Miguel Silva.</span>
+                      </a>
                     </div>
                       <!-- <p>Email: {{ email }}</p>
                       <p>Password: {{ password }}</p> -->
